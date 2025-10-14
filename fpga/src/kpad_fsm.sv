f20218fd9ba917753b192b8e813313cd46344d27
D. Sebastian Heredia <sebastianheredi4@gmail.com>
2025-10-13 21:37:46 -0700

    Update kpad_fsm.sv

--- fpga/src/kpad_fsm.sv
+++ fpga/src/kpad_fsm.sv
@@ -2,7 +2,7 @@
 // dheredia@g.hmc.edu
 // September 16, 2025
 
-// 
+// kpad_fsm.sv contains code to ensure that keypad button presses function as intended. 
 
 module kpad_fsm (
 	input logic			  clk,
@@ -15,15 +15,12 @@ module kpad_fsm (
 	);
 	
 	// State encoding
-	typedef enum logic [5:0] {	// Sufficient bits to cover 20 states
+	typedef enum logic [4:0] {	// Sufficient bits to cover 20 states
 		r0, r1, r2, r3,			// row
 		p0, p1, p2, p3,			// press
-		d0, d1, d2, d3,			// synced	(avoided S/s, too similar)
+		s0, s1, s2, s3,			// synced	(avoided S/s, too similar)
 		e0, e1, e2, e3,			// enable
-		w0, w1, w2, w3,			// wait
-		b0, b1, b2, b3,
-		q0, q1, q2, q3,			// time (fix timing with kpad_decoder)
-		t0, t1, t2, t3			// time (fix timing with kpad_decoder)
+		w0, w1, w2, w3			// wait
 	} state_t;
 	
 	state_t state, next_state;
@@ -39,11 +36,10 @@ module kpad_fsm (
 	
 	// Check that only 1 push button is active-HIGH
 	logic press;
-	// assign press = $onehot(col);	// Check if (press == 1) col goes HIGH (1) when pressed, so 0001
 	assign press = col[0] ^ col[1] ^ col[2] ^ col[3];
 	
 	logic [3:0] first_col;
-	
+
 	always_ff @(posedge clk) begin
 		if(reset == 0) begin
 			first_col <= 4'b0000;
@@ -53,6 +49,9 @@ module kpad_fsm (
 			first_col <= col;
 		end
 	end
+	
+	logic oneCol;
+	assign oneCol = (col & first_col) != 4'b0000;
 	
 	// LED debugging
 	// assign led [3:0] = row;
@@ -85,65 +84,47 @@ module kpad_fsm (
 					else next_state = r0;
 				end
 			
-	////////////////////////////////
-			
 			// Two counts behind to synchronize row logic with synchronized column logic
 			
 			// Row 0 pressed
-			p2: next_state = d2;		// Always (catch asynchronous column signal)
-			d2: next_state = e2; 		// Always (set sychronous column signal)
+			p2: next_state = s2;		// Always (catch asynchronous column signal)
+			s2: next_state = e2; 		// Always (set sychronous column signal)
 			e2: next_state = w2;		// Always (pulse enable after syncing)
 			
 			w2: begin
-					if ((col & first_col) != 4'b0000) next_state = w2;
-					else next_state = t2;
+					if (oneCol) next_state = w2;
+					else next_state = r2;
 				end
-	
-			t2: next_state = q2;
-			q2: next_state = b2;
-			b2: next_state = r1;
 				
 			// Row 1 pressed
-			p3: next_state = d3;
-			d3: next_state = e3;
+			p3: next_state = s3;
+			s3: next_state = e3;
 			e3: next_state = w3;
 			
 			w3: begin
-					if ((col & first_col) != 4'b0000) next_state = w3;
-					else next_state = t3;
+					if (oneCol) next_state = w3;
+					else next_state = r3;
 				end
-				
-			t3: next_state = q3;
-			q3: next_state = b3;
-			b3: next_state = r2;
 	
 			// Row 2 pressed
-			p0: next_state = d0;
-			d0: next_state = e0;
+			p0: next_state = s0;
+			s0: next_state = e0;
 			e0: next_state = w0;
 			
 			w0: begin
-					if ((col & first_col) != 4'b0000) next_state = w0;
-					else next_state = t0;
+					if (oneCol) next_state = w0;
+					else next_state = r0;
 				end
-				
-			t0: next_state = q0;
-			q0: next_state = b0;
-			b0: next_state = r3;
 			
 			// Row 3 pressed
-			p1: next_state = d1;
-			d1: next_state = e1;
+			p1: next_state = s1;
+			s1: next_state = e1;
 			e1: next_state = w1;
 			
 			w1: begin
-					if ((col & first_col) != 4'b0000) next_state = w1;
-					else next_state = t1;
+					if (oneCol) next_state = w1;
+					else next_state = r1;
 				end
-				
-			t1: next_state = q1;
-			q1: next_state = b1;
-			b1: next_state = r0;
 				
 			default: next_state = r0;	// Default back to the idle row loop
 		endcase
@@ -151,10 +132,10 @@ module kpad_fsm (
 	// FSM output logic
 	
 	always_comb begin
-		if 		((state == r0) || (state == p2) || (state == d2) || (state == e2) || (state == w2) || (state == t2)) row = 4'b0001;		// OFFSET states & TRUE row encoding
-		else if ((state == r1) || (state == p3) || (state == d3) || (state == e3) || (state == w3) || (state == t3)) row = 4'b0010;		// No flicker when any other buttons are pressed
-		else if ((state == r2) || (state == p0) || (state == d0) || (state == e0) || (state == w0) || (state == t0)) row = 4'b0100;
-		else if ((state == r3) || (state == p1) || (state == d1) || (state == e1) || (state == w1) || (state == t1)) row = 4'b1000;
+		if 		((state == r0) || (state == p2) || (state == s2) || (state == e2) || (state == w2)) row = 4'b0100; 	// Offset encoding row encoding to account for offset column syncing
+		else if ((state == r1) || (state == p3) || (state == s3) || (state == e3) || (state == w3)) row = 4'b1000;	
+		else if ((state == r2) || (state == p0) || (state == s0) || (state == e0) || (state == w0)) row = 4'b0001;
+		else if ((state == r3) || (state == p1) || (state == s1) || (state == e1) || (state == w1)) row = 4'b0010;
 		else row = 4'b0001;
 	end
 	
